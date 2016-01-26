035e6388813e8b1ebd7c4df23e259c0e7841a485
tommyvercetti <andriancrick@gmail.com>
2016-01-26 16:45:51 +0200

    replacing project

---
+++ lesson_01/lesson_01/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  lesson_01
+//
+//  Created by Andrian on 11/3/15.
+//  Copyright (c) 2015 Andrian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ lesson_01/lesson_01/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  lesson_01
+//
+//  Created by Andrian on 11/3/15.
+//  Copyright (c) 2015 Andrian. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
