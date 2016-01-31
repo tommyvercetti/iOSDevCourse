4375f510d5b79376e53d74d58936f535b4f16bd6
tommyvercetti <andriancrick@gmail.com>
2016-01-31 22:14:15 +0200

    finished

---
+++ lesson_04(3.2)/ParametersTest/ParametersTest/Object.h
@@ -0,0 +1,13 @@
+//
+//  Object.h
+//  ParametersTest
+//
+//  Created by Admin on 1/31/16.
+//  Copyright © 2016 Andrian Kryk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Object : NSObject
+
+@end
---
+++ lesson_04(3.2)/ParametersTest/ParametersTest/Object.m
@@ -0,0 +1,13 @@
+//
+//  Object.m
+//  ParametersTest
+//
+//  Created by Admin on 1/31/16.
+//  Copyright © 2016 Andrian Kryk. All rights reserved.
+//
+
+#import "Object.h"
+
+@implementation Object
+
+@end
