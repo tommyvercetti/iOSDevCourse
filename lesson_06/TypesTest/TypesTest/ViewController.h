45fcb3a636e252ef13de075c5263778f64b61d31
tommyvercetti <andriancrick@gmail.com>
2016-02-17 14:31:12 +0200

    lesson_06 started

---
+++ lesson_06/TypesTest/TypesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TypesTest
+//
+//  Created by Andrian on 2/17/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
