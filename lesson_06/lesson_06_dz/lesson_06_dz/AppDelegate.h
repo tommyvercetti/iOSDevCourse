3265a9a12bdfb8a73192947e2bb2efb2bf048fe6
tommyvercetti <andriancrick@gmail.com>
2016-02-17 17:45:35 +0200

    lesson_06_dz started

---
+++ lesson_06/lesson_06_dz/lesson_06_dz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lesson_06_dz
+//
+//  Created by Andrian on 2/17/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
