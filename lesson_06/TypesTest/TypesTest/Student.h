f312d6a8ec44a3190314b454901f13f07c7fa05a
tommyvercetti <andriancrick@gmail.com>
2016-02-17 15:27:47 +0200

    enum

---
+++ lesson_06/TypesTest/TypesTest/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  TypesTest
+//
+//  Created by Andrian on 2/17/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ lesson_06/TypesTest/TypesTest/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  TypesTest
+//
+//  Created by Andrian on 2/17/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
