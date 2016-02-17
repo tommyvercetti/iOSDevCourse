aaba770be85d48d785949c1a2264f027c9656323
tommyvercetti <andriancrick@gmail.com>
2016-02-17 18:20:53 +0200

    protoclass added

--- lesson_06/lesson_06_dz/lesson_06_dz/ProtoClass.h
+++ lesson_06/lesson_06_dz/lesson_06_dz/ProtoClass.h
@@ -1,13 +1,13 @@
 //
-//  StarCraft.m
+//  ProtoClass.h
 //  lesson_06_dz
 //
 //  Created by Andrian on 2/17/16.
 //  Copyright © 2016 Andrian. All rights reserved.
 //
 
-#import "StarCraft.h"
+#import <Foundation/Foundation.h>
 
-@implementation StarCraft
+@interface ProtoClass : NSObject
 
 @end
--- lesson_06/lesson_06_dz/lesson_06_dz/ProtoClass.m
+++ lesson_06/lesson_06_dz/lesson_06_dz/ProtoClass.m
@@ -1,13 +1,13 @@
 //
-//  StarCraft.h
+//  ProtoClass.m
 //  lesson_06_dz
 //
 //  Created by Andrian on 2/17/16.
 //  Copyright © 2016 Andrian. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "ProtoClass.h"
 
-@interface StarCraft : NSObject
+@implementation ProtoClass
 
 @end
