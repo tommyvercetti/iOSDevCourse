87bce9645f0d4722c7cd41a8a8262aa283f98998
tommyvercetti <andriancrick@gmail.com>
2016-02-19 17:27:01 +0200

    5

---
+++ lesson_06/lesson_06_dz/lesson_06_dz/Statistics.h
@@ -0,0 +1,13 @@
+//
+//  Statistics.h
+//  lesson_06_dz
+//
+//  Created by Andrian on 2/19/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Statistics : NSObject
+
+@end
---
+++ lesson_06/lesson_06_dz/lesson_06_dz/Statistics.m
@@ -0,0 +1,13 @@
+//
+//  Statistics.m
+//  lesson_06_dz
+//
+//  Created by Andrian on 2/19/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import "Statistics.h"
+
+@implementation Statistics
+
+@end
