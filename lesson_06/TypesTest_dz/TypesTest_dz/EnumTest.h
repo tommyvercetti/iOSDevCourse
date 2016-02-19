6b295ef73bb8e378b641fd872a0b24a4f308db42
tommyvercetti <andriancrick@gmail.com>
2016-02-19 12:01:15 +0200

    f

---
+++ lesson_06/TypesTest_dz/TypesTest_dz/EnumTest.h
@@ -0,0 +1,13 @@
+//
+//  EnumTest.h
+//  TypesTest_dz
+//
+//  Created by Andrian on 2/19/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EnumTest : NSObject
+
+@end
---
+++ lesson_06/TypesTest_dz/TypesTest_dz/EnumTest.m
@@ -0,0 +1,13 @@
+//
+//  EnumTest.m
+//  TypesTest_dz
+//
+//  Created by Andrian on 2/19/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import "EnumTest.h"
+
+@implementation EnumTest
+
+@end
