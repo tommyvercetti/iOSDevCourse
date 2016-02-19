aa4b72bd7b0343381658d5626cd13ff134295ffd
tommyvercetti <andriancrick@gmail.com>
2016-02-19 11:58:33 +0200

    new 06 lesson dz

---
+++ lesson_06/TypesTest_dz/TypesTest_dz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TypesTest_dz
+//
+//  Created by Andrian on 2/19/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
