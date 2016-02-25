0c9e7a79f3730a5643936507cd4968bf10a848d1
tommyvercetti <andriancrick@gmail.com>
2016-02-25 11:30:59 +0200

    lesson 07 started

---
+++ lesson_07/ProtocolsTest/ProtocolsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProtocolsTest
+//
+//  Created by Andrian on 2/25/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lesson_07/ProtocolsTest/ProtocolsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtocolsTest
+//
+//  Created by Andrian on 2/25/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
