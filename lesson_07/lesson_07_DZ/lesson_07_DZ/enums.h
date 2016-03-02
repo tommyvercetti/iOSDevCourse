50a05943ecb474f5081aef04ac29c5026846c626
tommyvercetti <andriancrick@gmail.com>
2016-03-02 17:56:18 +0200

    s

---
+++ lesson_07/lesson_07_DZ/lesson_07_DZ/enums.h
@@ -0,0 +1,13 @@
+//
+//  enums.h
+//  lesson_07_DZ
+//
+//  Created by Andrian on 3/2/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol enums <NSObject>
+
+@end
