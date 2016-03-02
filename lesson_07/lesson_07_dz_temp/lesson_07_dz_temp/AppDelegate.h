e6e80ccd9eb974eeac3c9be531a103aef9f683ba
tommyvercetti <andriancrick@gmail.com>
2016-03-02 18:00:15 +0200

    tepm dz created

---
+++ lesson_07/lesson_07_dz_temp/lesson_07_dz_temp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lesson_07_dz_temp
+//
+//  Created by Andrian on 3/2/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lesson_07/lesson_07_dz_temp/lesson_07_dz_temp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lesson_07_dz_temp
+//
+//  Created by Andrian on 3/2/16.
+//  Copyright © 2016 Andrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
