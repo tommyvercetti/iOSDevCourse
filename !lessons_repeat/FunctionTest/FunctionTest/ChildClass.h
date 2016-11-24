f7fd26884f0ac0a947c3989630956a006253db73
tommyvercetti <andriancrick@gmail.com>
2016-11-24 16:56:44 +0200

    lesson2 complete

---
+++ !lessons_repeat/FunctionTest/FunctionTest/ChildClass.h
@@ -0,0 +1,13 @@
+//
+//  ChildClass.h
+//  FunctionTest
+//
+//  Created by Andrian Kryk on 11/24/16.
+//  Copyright © 2016 Andrian Kryk. All rights reserved.
+//
+
+#import "ParentClass.h"
+
+@interface ChildClass : ParentClass
+
+@end
---
+++ !lessons_repeat/FunctionTest/FunctionTest/ChildClass.m
@@ -0,0 +1,13 @@
+//
+//  ChildClass.m
+//  FunctionTest
+//
+//  Created by Andrian Kryk on 11/24/16.
+//  Copyright © 2016 Andrian Kryk. All rights reserved.
+//
+
+#import "ChildClass.h"
+
+@implementation ChildClass
+
+@end
