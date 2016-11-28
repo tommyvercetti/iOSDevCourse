0f28ba4f40a1200d6e2a9b01a8ad71bc70c10ad8
tommyvercetti <andriancrick@gmail.com>
2016-11-28 16:39:03 +0200

    lesson 3 repeat

---
+++ !lessons_repeat/Properties/Properties/Properties/Boxer.h
@@ -0,0 +1,13 @@
+//
+//  Boxer.h
+//  Properties
+//
+//  Created by Andrian Kryk on 11/28/16.
+//  Copyright © 2016 Andrian Kryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Boxer : ParentClass
+
+@end
---
+++ !lessons_repeat/Properties/Properties/Properties/Boxer.m
@@ -0,0 +1,13 @@
+//
+//  Boxer.m
+//  Properties
+//
+//  Created by Andrian Kryk on 11/28/16.
+//  Copyright © 2016 Andrian Kryk. All rights reserved.
+//
+
+#import "Boxer.h"
+
+@implementation Boxer
+
+@end
